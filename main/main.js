6b38804fb9a65eeebe89b54abb0e759a7d83664e
your name <quangdungdao20@gmail.com>
2022-11-30 18:24:00 +0700

    the band

---
+++ main/main.js
@@ -0,0 +1,15 @@
+
+// var header = document.getElementById('header');
+// var mobileMenu = document.getElementsByClassName('mobile-menu');
+
+// header.onclick = function(){
+//     var isClosed = header.clientHeight=== 48 ;
+//     if(isClosed) {
+//         header.style.height = 'auto';
+//     } 
+//     else{
+//         header.style.height = '48px';
+
+//     }
+//     console.log(header.clientHeight);
+// }
