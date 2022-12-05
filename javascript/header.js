028075aaf8d076545d043de0c39fe488a8a7adf6
your name <quangdungdao20@gmail.com>
2022-12-06 02:55:29 +0700

    fix menu in reponsive

---
+++ javascript/header.js
@@ -0,0 +1,28 @@
+
+// var Nav = document.querySelector('#nav');
+// var NavContent = document.querySelectorAll('#nav .nav-content');
+// var MbMenu = document.querySelector('#mobile-menu');
+// var ListMore = document.querySelectorAll('.list-items');
+// var NavLength = NavContent.length
+// console.log(Nav.clientWidth);
+// if(Nav.clientWidth < '750'){
+    
+//     MbMenu.onclick = function(){
+//         if(Nav.clientHeight == '48'){
+//             for(var i=0; i < NavLength; i++){
+//                 NavContent[i].style.display = 'inline-block'; 
+//             }
+//         }
+       
+
+//     }
+//         for(var i=0; i < NavLength; i++){
+//             var NavC = NavContent[i];
+//             NavC.onclick = function(){
+//                 for(var j=0; j < NavLength; j++){
+//                     NavContent[j].style.display = 'none'; 
+//                 }
+                
+//             }
+//         }
+// }
