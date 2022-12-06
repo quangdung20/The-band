d2cb7dbcbea866dadaefe6c667b2a9b6ed7ff7e2
your name <quangdungdao20@gmail.com>
2022-12-07 03:50:21 +0700

    fix slider

--- javascript/header.js
+++ javascript/header.js
@@ -1,28 +1,31 @@
+var Nav = document.querySelector('#nav');
+var NavContent = document.querySelectorAll('#nav .nav-content');
+var MbMenu = document.querySelector('#mobile-menu');
+var ListMore = document.querySelectorAll('.list-items');
+var NavLength = NavContent.length;
+MbMenu.onclick = function(){
+    if(Nav.clientHeight == '48'){
+        for(var i=0; i < NavLength; i++){
+            NavContent[i].style.display = 'inline-block'; 
+            }
+        }
+        
+    else {
+        for(var i=0; i < NavLength; i++){
+            NavContent[i].style.display = ''; 
+        }
+    }
+}
 
-// var Nav = document.querySelector('#nav');
-// var NavContent = document.querySelectorAll('#nav .nav-content');
-// var MbMenu = document.querySelector('#mobile-menu');
-// var ListMore = document.querySelectorAll('.list-items');
-// var NavLength = NavContent.length
-// console.log(Nav.clientWidth);
-// if(Nav.clientWidth < '750'){
-    
-//     MbMenu.onclick = function(){
-//         if(Nav.clientHeight == '48'){
-//             for(var i=0; i < NavLength; i++){
-//                 NavContent[i].style.display = 'inline-block'; 
-//             }
-//         }
-       
-
-//     }
-//         for(var i=0; i < NavLength; i++){
-//             var NavC = NavContent[i];
-//             NavC.onclick = function(){
-//                 for(var j=0; j < NavLength; j++){
-//                     NavContent[j].style.display = 'none'; 
-//                 }
-                
-//             }
-//         }
-// }
+for(var i=0; i < NavLength; i++){
+     var NavC = NavContent[i];
+     NavC.onclick = function(){
+        var NavWidth = Nav.clientWidth;
+        if(NavWidth < '750'){
+            for(var j=0; j < NavLength; j++){
+                NavContent[j].style.display = 'none'; 
+            }
+        }
+         
+    }
+}
